8ccc0e6947df9abb6414c2d5dc4ec8a5295b7efe
NinoDS <75518629+NinoDS@users.noreply.github.com>
2022-08-20 16:14:14 +0200

    Implement GET for lockers

--- index.js
+++ index.js
@@ -1,9 +1,82 @@
 import Database from "./database.js";
 import express from "express";
 
+const users = new Database("users.json");
+const accounts = new Database("accounts.json");
+
 const app = express();
 const port = 3000;
 
-/* ... */
+app.use(express.json());
+
+/**
+ * Login user and return it.
+ * @param {String} username Username
+ * @param {String} password Password
+ * @returns {Promise<Object>} Account
+ */
+async function login(username, password) {
+	const user = await users.get(username);
+	if (!user) {
+		throw new Error("User not found");
+	}
+	if (user.password !== password) {
+		throw new Error("Invalid password");
+	}
+	return user;
+}
+
+/**
+ * Authentication middleware.
+ */
+app.use(async (req, res, next) => {
+	const { username, password } = req.body;
+	if (!username || !password) {
+		return res.status(400).send("Missing username or password");
+	}
+	const user = await login(username, password);
+	const path = req.path.substring(1).split("/");
+	if (user.allowed.includes(path[0])) {
+		next();
+	} else {
+		return res.status(403).send("Forbidden");
+	}
+});
+
+/**
+ * Filter out sepa data if not allowed.
+ * @param {Object} locker Locker
+ * @param {Object} user User
+ * @returns {Object} Filtered locker
+ */
+function filterLocker(locker, user) {
+	if (!user.sepa) {
+		delete locker.sepa;
+	}
+	return locker;
+}
+
+/**
+ * Get all lockers.
+ */
+app.get("/lockers", async (req, res) => {
+	const lockers = await accounts.getAll();
+	for (let locker of lockers) {
+		locker = filterLocker(locker, req.user);
+	}
+	res.send(lockers);
+});
+
+/**
+ * Get locker by id.
+ */
+app.get("/lockers/:id", async (req, res) => {
+	let locker = await accounts.get(req.params.id);
+	if (!locker) {
+		return res.status(404).send("Locker not found");
+	}
+	locker = filterLocker(locker, req.user);
+	res.send(locker);
+});
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
